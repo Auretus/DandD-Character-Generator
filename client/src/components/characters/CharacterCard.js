9c88c8b89f0e74faf83f6efcf4a81740c4410336
Alex Joel <alexjoeldelgado@gmail.com>
2020-06-01 21:33:23 -0400

    Fixed ClassName and expanded Createjs

--- client/src/components/characters/CharacterCard.js
+++ client/src/components/characters/CharacterCard.js
@@ -4,17 +4,17 @@ import { Link } from "react-router-dom";
 const CharacterCard = () => {
     return (
         <div className="container">
-            <div class="card col m6 s12 black-text charcard">
-                <div class="card-image">
-                    <img class="activator responsive-img" src="images/ysoki.jpg" style={{height: "500px"}} alt="temp" />
+            <div className="card col m6 s12 black-text charcard">
+                <div className="card-image">
+                    <img className="activator responsive-img" src="images/ysoki.jpg" style={{height: "500px"}} alt="temp" />
                 </div>
-                <div class="card-content center">
-                    <span class="card-title activator grey-text text-darken-4">Quig<i class="material-icons right small">chevron_right</i></span>
+                <div className="card-content center">
+                    <span className="card-title activator grey-text text-darken-4">Quig<i className="material-icons right small">chevron_right</i></span>
                     <p><Link to="/charactersheet">Click here for Full Character Sheet</Link></p>
                 </div>
-                <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">Quig<i class="material-icons right">close</i></span>
-                    <div class="card-content left-align">
+                <div className="card-reveal">
+                    <span className="card-title grey-text text-darken-4">Quig<i className="material-icons right">close</i></span>
+                    <div className="card-content left-align">
                         <div className="col s6">
                             <p><b>Class/Level:</b> Mechanic Lvl 1</p>
                             <p><b>Race:</b> Ysoki</p>
@@ -30,12 +30,12 @@ const CharacterCard = () => {
                             <p><b>Player:</b> </p>
                         </div>
                     </div>
-                    <div class="card-tabs">
-                        <ul class="tabs tabs-fixed-width">
-                            <li class="tab">Ability Scores</li>
+                    <div className="card-tabs">
+                        <ul className="tabs tabs-fixed-width">
+                            <li className="tab">Ability Scores</li>
                         </ul>
                     </div>
-                    <div class="card-content">
+                    <div className="card-content">
                         <div id="test4">
                             <div className="col s6">
                                 <p><b>Strength:</b> 10 </p>
